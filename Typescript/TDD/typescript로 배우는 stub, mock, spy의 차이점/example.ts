e5c60802f8490c16d265632d033451019840f61c
CharmingKyu <45731301+loadkrnis@users.noreply.github.com>
2022-02-02 12:51:11 +0900

    추가 example

--- Typescript/TDD/typescript로 배우는 stub, mock, spy의 차이점/example.ts
+++ Typescript/TDD/typescript로 배우는 stub, mock, spy의 차이점/example.ts
@@ -1,3 +1,10 @@
+class MailService {
+  send(message: string): void {
+    // 메일 전송 로직
+    console.log(message);
+  }
+}
+
 class Order {
   readonly name: string;
   readonly price: number;
@@ -17,22 +24,8 @@ class Order {
   }
 }
 
-class MailService {
-  send(message: string): void {
-    // 메일 전송 로직
-    console.log(message);
-  }
-
-  numberSent(): number {
-    // DB에서 메일 전송한 횟수 불러오는 로직
-    const result = 10;
-
-    return result;
-  }
-}
-
 class MailServiceStub extends MailService {
-  readonly messages: Array<string>;
+  messages: Array<string>;
 
   constructor() {
     super();
@@ -43,12 +36,79 @@ class MailServiceStub extends MailService {
     this.messages.push(message);
   }
 
-  override numberSent(): number {
+  numberSent(): number {
     return this.messages.length;
   }
 }
 
-describe('Order', function () {
-  // confirm 했을 때 메일을 발송한다.
-  // confirm 했을 때 메일을 발송한다.
+describe('Order (stub)', function () {
+  it('confirm 했을 때 메일을 발송한다.', function () {
+    // given
+    const order = new Order('name', 15000);
+    const mailService = new MailServiceStub();
+    order.setMailer(mailService);
+
+    // when
+    order.confirm();
+
+    // then
+    expect(mailService.numberSent()).toBe(1);
+  });
+});
+
+describe('Order (mock)', function () {
+  beforeEach(() => jest.clearAllMocks());
+
+  it('confirm 했을 때 메일을 발송한다.', function () {
+    // given
+    const mailService = new MailService();
+    mailService.send = jest.fn();
+    const mailServiceSend = mailService.send;
+    const order = new Order('name', 15000);
+    order.setMailer(mailService);
+
+    // when
+    order.confirm();
+
+    // then
+    expect(mailServiceSend).toBeCalledTimes(1);
+  });
+});
+
+describe('Order (spy)', function () {
+  beforeEach(() => jest.clearAllMocks());
+
+  it('confirm 했을 때 메일을 발송한다.', function () {
+    // given
+    const mailService = new MailService();
+    const spyMailService = jest.spyOn(mailService, 'send');
+    const order = new Order('name', 15000);
+    order.setMailer(mailService);
+
+    // when
+    order.confirm();
+
+    // then
+    expect(spyMailService).toBeCalledTimes(1);
+  });
+});
+
+class Car {
+  drive() {
+    console.log('부릉');
+    return '부릉';
+  }
+}
+
+it('spy', async () => {
+  // given
+  const car = new Car();
+  const spyCar = jest.spyOn(car, 'drive');
+
+  // when
+  const result = car.drive();
+
+  // then
+  expect(result).toBe('부릉');
+  expect(spyCar).toBeCalledTimes(1);
 });
